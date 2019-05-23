0e9a72e004e1d308f8bf5557b4ee64344ea12c9d
田露华 <tianluhua@booyue.com.cn>
2019-05-23 09:09:39 +0800

    init project

---
+++ eureka-server/src/main/java/com/booyue/tlh/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,13 @@
+package com.booyue.tlh.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+}
