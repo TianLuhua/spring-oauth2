57fc1ce7341636c942e711698a8bc1582ba30671
田露华 <tianluhua@booyue.com.cn>
2019-05-23 09:55:20 +0800

    add authorization-server

--- authorization-server/src/main/java/com/booyue/tlh/authorizationserver/AuthorizationServerApplication.java
+++ authorization-server/src/main/java/com/booyue/tlh/authorizationserver/AuthorizationServerApplication.java
@@ -2,12 +2,22 @@ package com.booyue.tlh.authorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class AuthorizationServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "authorization-server";
     }
 
 }
--- eureka-server/src/main/java/com/booyue/tlh/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/booyue/tlh/eurekaserver/EurekaServerApplication.java
@@ -2,8 +2,10 @@ package com.booyue.tlh.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class EurekaServerApplication {
 
     public static void main(String[] args) {
